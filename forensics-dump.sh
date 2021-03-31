886c448671079bda8d2df269020680769b89945f
Sergey Marunich <marunich.s@gmail.com>
2021-03-31 12:41:28 -0400

    Update forensics-dump.sh

--- forensics-dump.sh
+++ forensics-dump.sh
@@ -49,7 +49,7 @@ fi
 UUID=$(${_kubectl} get vmis ${vm} -n ${namespace} --no-headers -o custom-columns=METATADA:.metadata.uid) 
 POD=$(${_kubectl} get pods -n ${namespace} -l kubevirt.io/created-by=${UUID} --no-headers -o custom-columns=NAME:.metadata.name)
 _exec="${_kubectl} exec  ${POD} -n ${namespace} -c compute --"
-_virtctl="virtctl --namespace ${namespace}
+_virtctl="virtctl --namespace ${namespace}"
 
  if [ "${action}" == "pause" ]; then
     ${_virtctl} pause vm ${vm}
@@ -69,7 +69,7 @@ elif [ "${action}" == "unpause" ]; then
     ${_exec} sed -i 's[unix_sock_dir = "/var/run/libvirtt"[#unix_sock_dir = "/var/run/libvirt"[' /etc/libvirt/libvirtd.conf
     LIBVIRT_PID=$(${_exec} bash -c 'pidof -s libvirtd')
     ${_exec} kill ${LIBVIRT_PID}
-    _virsh="${_exec} virsh"
+    _virsh="${_exec} virsh" 
     ${_exec} rm -rf /var/run/libvirtt
     sleep ${timeout}
     ${_virtctl} unpause vm ${vm}
